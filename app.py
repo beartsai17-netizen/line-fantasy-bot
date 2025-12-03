02651435a9044723183206bca409232f8e5e247d
beartsai17-netizen <beartsai17@gmail.com>
2025-12-03 17:37:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from openai import OpenAI
 
+import requests
+
 import os
 
 from flask import Flask, request, abort
@@ -151,6 +153,7 @@ else:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
 
